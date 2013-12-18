dd62224b0aeab56b99cb37409ede3ac81adeafe5
Ashley Sarver <asarver1@gmail.com>
2013-12-18 01:09:26 -0500

    asks for number to notify

--- src/com/businesstime/MainActivity.java
+++ src/com/businesstime/MainActivity.java
@@ -1,8 +1,13 @@
 package com.businesstime;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
@@ -10,12 +15,25 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		((EditText)findViewById(R.id.phone_number)).setOnKeyListener(
+		        new OnKeyListener() { // .OnEditorActionListener() {
+
+					@Override
+					public boolean onKey(View v, int keyCode, KeyEvent event) {
+						if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+							InputMethodManager imm = (InputMethodManager) getSystemService(MainActivity.INPUT_METHOD_SERVICE);
+							imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+							return true;
+						}
+						return false;
+					}});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
