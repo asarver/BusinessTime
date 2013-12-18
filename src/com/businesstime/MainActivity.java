0b3c5aa5ab5048ea16dc059acf09bbedeef610db
Ashley Sarver <asarver1@gmail.com>
2013-12-18 01:33:35 -0500

    can select from contacts

--- src/com/businesstime/MainActivity.java
+++ src/com/businesstime/MainActivity.java
@@ -3,7 +3,10 @@ package com.businesstime;
 import android.app.Activity;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.telephony.SmsManager;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -11,6 +14,7 @@ import android.view.View;
 import android.view.View.OnKeyListener;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -34,15 +38,27 @@ public class MainActivity extends Activity {
 					}});
 	}
 	
+	public void selectFromContacts(View v) {
+		// user BoD suggests using Intent.ACTION_PICK instead of .ACTION_GET_CONTENT to avoid the chooser
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        // BoD con't: CONTENT_TYPE instead of CONTENT_ITEM_TYPE
+        intent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE);
+        startActivityForResult(intent, 1);
+	}
+	
 	private void sendSMS()
     {        
 		String phoneNumber = ((EditText)findViewById(R.id.phone_number)).getText()+"";
 		String message = "I'm in the mood!";
-        PendingIntent pi = PendingIntent.getActivity(this, 0,
-            new Intent(this, SmsManager.class), 0);                
-        SmsManager sms = SmsManager.getDefault();
-        sms.sendTextMessage(phoneNumber, null, message, pi, null);        
+		sendSMS(message, phoneNumber);        
     } 
+	
+	private void sendSMS(String message, String phoneNumber) {
+		PendingIntent pi = PendingIntent.getActivity(this, 0,
+	            new Intent(this, SmsManager.class), 0);                
+	    SmsManager sms = SmsManager.getDefault();
+	    sms.sendTextMessage(phoneNumber, null, message, pi, null);
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -51,4 +67,29 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (data != null) {
+	        Uri uri = data.getData();
+
+	        if (uri != null) {
+	            Cursor c = null;
+	            try {
+	                c = getContentResolver().query(uri, new String[]{ 
+	                            ContactsContract.CommonDataKinds.Phone.NUMBER},
+	                        null, null, null);
+
+	                if (c != null && c.moveToFirst()) {
+	                    String number = c.getString(0);
+	                    ((EditText)findViewById(R.id.phone_number)).setText(number);
+	                    sendSMS("Let me know if you get this", number);
+	                }
+	            } finally {
+	                if (c != null) {
+	                    c.close();
+	                }
+	            }
+	        }
+	    }
+	}
 }
