f1e12d8ab9a42e5591e363f0864bc669063a55e1
Ashley Sarver <asarver1@gmail.com>
2013-12-18 01:16:30 -0500

    sends a text message to the number inputted

--- src/com/businesstime/MainActivity.java
+++ src/com/businesstime/MainActivity.java
@@ -1,7 +1,10 @@
 package com.businesstime;
 
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
@@ -24,11 +27,22 @@ public class MainActivity extends Activity {
 						if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
 							InputMethodManager imm = (InputMethodManager) getSystemService(MainActivity.INPUT_METHOD_SERVICE);
 							imm.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+							sendSMS();
 							return true;
 						}
 						return false;
 					}});
 	}
+	
+	private void sendSMS()
+    {        
+		String phoneNumber = ((EditText)findViewById(R.id.phone_number)).getText()+"";
+		String message = "I'm in the mood!";
+        PendingIntent pi = PendingIntent.getActivity(this, 0,
+            new Intent(this, SmsManager.class), 0);                
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, pi, null);        
+    } 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
